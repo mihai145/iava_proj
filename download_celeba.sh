e475c60d5466aa9585424fc5973907dcede99bde
mihai145 <opreamihai01@yahoo.com>
2024-04-22 15:18:29 +0300

    remove outdated

--- download_celeba.sh
+++
@@ -1,5 +0,0 @@
-URL=https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0
-ZIP_FILE=./drive/MyDrive/StarGAN/data/celeba.zip
-wget -N $URL -O $ZIP_FILE
-unzip $ZIP_FILE -d ./drive/MyDrive/StarGAN/data/
-rm $ZIP_FILE
